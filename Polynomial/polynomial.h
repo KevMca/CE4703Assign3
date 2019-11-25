89b53637e4580541f14f1af7147bf9fccbddffa4
Kevin McAndrew <Kevin@Michaels-MacBook-Pro.local>
2019-11-25 17:42:22 +0000

    added polyDelete() and coeffDelete()

--- Polynomial/polynomial.h
+++ Polynomial/polynomial.h
@@ -8,35 +8,48 @@
 
 typedef struct
 {
-  	double coefficient;
-	int order;
+  	double coefficient; // Coefficient for order
+	int order; // Position in polynomial (x^order)
 } polyData;
 
 typedef struct n
 {
-  	polyData d;
-  	struct n *next;
+  	polyData d; // Coefficient and order
+  	struct n *next; // Next coefficient
 } polyNode;
 
 
 typedef struct
 {
-  	polyNode *head;
-  	polyNode *current;
-  	int order; //order of the coefficient
+  	polyNode *head; // Head of the polynomial
+  	polyNode *current; // Current node cursor
+  	int order; // Order of the coefficient
 } polyList;
 
-
-polyList *polyCreate(); //creates a polynomial
+// Polynomial editing operation
+// Creates an empty polynomial
+polyList *polyCreate();
+// Fills a polynomial with coefficients
 int fillPoly(polyList *poly, int order, double arr[]);
-polyNode *newCoeff(double coefficient, int order); //adds a node to a polynomial
-int deleteNode(); //deletes a polynomial
-polyList add(polyList *poly1, polyList *poly2); //adds two polynomials together
-polyList subtract(polyList *poly1, polyList *poly2); //subtracts 2 polynomials
-polyList multiply(polyList *poly1); //multiplies a polynomial by a double
-polyList divide(polyList *poly1); //divides a polynomial by a double
-polyList normalise(polyList *poly1); //normalises a polynomial
-int order(polyList *poly1); //returns the highest order of the polynomial 
-void print(polyList *poly1); //prints out the polynomial to stdout
+// Adds a coefficient to a polynomial
+polyNode *newCoeff(double coefficient, int order);
+// Deletes a polynomial coefficient
+int deleteCoeff(polyNode *toDelete);
+
+// Polynomial operations
+// Adds two polynomials together
+polyList add(polyList *poly1, polyList *poly2);
+// Subtracts 2 polynomials
+polyList subtract(polyList *poly1, polyList *poly2);
+// Multiplies a polynomial by a doubles
+polyList multiply(polyList *poly1);
+// Divides a polynomial by a double
+polyList divide(polyList *poly1);
+// Normalises a polynomial
+polyList normalise(polyList *poly1);
+// Returns the highest order of the polynomial
+int order(polyList *poly1);
+// Prints out the polynomial to stdout 
+void print(polyList *poly1);
 
 #endif
