bfd7017e9044f3dcd1935ef21578add6d4e318c8
Kevin McAndrew <Kevin@Michaels-MacBook-Pro.local>
2019-11-25 15:32:37 +0000

    updated polynomial.h for create() and addCoeff()

--- Polynomial/polynomial.h
+++ Polynomial/polynomial.h
@@ -8,45 +8,33 @@
 
 typedef struct
 {
+  	double coefficient;
+	int order;
+} polyData;
+
+typedef struct n
+{
+  	polyData d;
+  	struct n *next;
+} polyNode;
+
+
+typedef struct
+{
   	polyNode *head;
   	polyNode *current;
   	int order; //order of the coefficient
 } polyList;
 
-typedef struct
-{
-  	polyData d;
-  	struct n *next
-} polyNode;
-
-typedef struct
-{
-  	double coefficient;
-	int order;
-} polyData;
-
-
-
-int create(polyList *poly1, order, double array()); //creates a polynomial
-
-int addNode(); //adds a node to a polynomial
-
+polyList *create(int order, double arr[]); //creates a polynomial
+polyNode *addCoeff(double coefficient, polyNode *current); //adds a node to a polynomial
 int deleteNode(); //deletes a polynomial
-
 polyList add(polyList *poly1, polyList *poly2); //adds two polynomials together
-
 polyList subtract(polyList *poly1, polyList *poly2); //subtracts 2 polynomials
-
 polyList multiply(polyList *poly1, polyList *poly2); //multiplies 2 polynomials
-
 polyList divide(polyList *poly1, polyList poly2); //divides 2 polynomials
-
 polyList normalise(polyList *poly1); //normalises a polynomial
-
 int order(polyList *poly1); //returns the highest order of the polynomial 
-
 void print(polyList *poly1); //prints out the polynomial to stdout
-
-
 
 #endif
