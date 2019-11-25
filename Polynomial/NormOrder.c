7bde08c35f4fc094f19332a84f8d8094b4bb6103
AaronNaylor <aaronnaylor99@gmail.com>
2019-11-25 16:01:34 +0000

    NormOrder.c

--- Polynomial/NormOrder.c
+++ Polynomial/NormOrder.c
@@ -1,30 +1,27 @@
 polyList normalise(polyList *poly1){
-  polyList norm;
-  double highCoeff;
-  int order;
-  order = order(poly1);
-  for(int i =0; i < order; i++)
+  polyList norm; // variable to store value before returned
+  double highCoeff; // stores coefficient of highest order
+  int order; // stores highest order
+  order = order(poly1); // calls order function to find highest order
+  for(int i =0; i < order; i++) // for loop from 0 to order in increments of 1
     {
-      *poly1->current->next;
-      current = poly1->current->next;
+      current = poly1->current->next; // sets current to next value
     }
 
-  highCoeff = poly1->current->d.coefficient; 
-  norm =  polyList divide(poly1, highCoeff);
-  return norm;
+  highCoeff = poly1->current->d.coefficient; // stores coefficient
+  norm =  polyList divide(poly1, highCoeff); // calls dividing function
+  return norm; // returns coefficient of highest order 
 }
 
 int order(polyList *poly1){
-  int length;
-  for(int i=0; i =< (poly1->order); i ++)
+  int length; // variable to store highest order in polyList
+  for(int i=0; i =< (poly1->order); i ++) // for loop from 0 to length of polylist 
     {
-      if(poly1->current->d.coefficient !=0)
+      if(poly1->current->d.coefficient !=0) // if poly1's coefficient is not equal to 0 
 	{
-	  
-	  length = (poly1List->order);
-	}
-      *poly1->current->next;
-      current = poly1->current->next;
+	  length = i; // length is highest order that is not equal to 0
+	} 
+      current = poly1->current->next; // sets current to next value
     }
-  return length;
+  return length; // returns most significant order
 }
