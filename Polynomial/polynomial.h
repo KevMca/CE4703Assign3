e62449e92e77cf7c06604e55e8a24b86410f3c0b
Stephen Gannon <stephen.gannon79@gmail.com>
2019-11-25 03:29:33 -0800

    added polynomial.h

---
+++ Polynomial/polynomial.h
@@ -0,0 +1,27 @@
+//polynomial.h
+//author: Stephen Gannon
+//date: 25/11/19
+//purpose: To declare all functions and data structures
+
+#ifndef HEADER_FILEPOLY
+#define HEADER_FILEPOLY
+
+typedef struct
+{
+  polyNode *head;
+  polyNode *current;
+  int order;
+} polyList;
+
+typedef struct
+{
+  polyData d;
+  struct n *next
+} polyNode;
+
+typedef struct
+{
+  
+}
+
+#endif
