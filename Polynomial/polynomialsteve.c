61233ba7723290cb3183bb8acf8b1a201ec3d046
Stephen Gannon <stephen.gannon79@gmail.com>
2019-11-25 07:04:17 -0800

    added polynomialsteve.c

--- Polynomial/polynomialsteve.c
+++ Polynomial/polynomialsteve.c
@@ -10,27 +10,25 @@
 polyList multiply(polyList *poly, double multiplyer)
 {
 	polyList polyMul;
-	int ord = order(&poly)
+	int ord = order(&poly);
 	for(int i = 0; i <= ord; i++)
 	{
 		polyMul.current->d.coefficient = *poly->current->d.coefficient*multiplyer;
 		addNode(...);
 		polyMul.current = polyMul.current->next;
-		return polyMul;
 	}
-
+	return polyMul;
 }
 
 polyList divide(polyList *poly, double divider)
 {
 	polyList polyDiv;
-	int ord = order(&poly)
+	int ord = order(&poly);
 	for(int i = 0; i <= ord; i++)
 	{
 		polyDiv.current->d.coefficient = *poly->current->d.coefficient/divider;
 		addNode(...);
 		polyDiv.current = polyDiv.current->next;
-		return poly3;
 	}
-
+	return poly3;
 }
