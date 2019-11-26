2973ca42df5b994c46926585b2b06299e6d6d241
Kevin McAndrew <Kevin@Michaels-MacBook-Pro.local>
2019-11-26 11:40:14 +0000

    finished print

--- Polynomial/Print.c
+++ Polynomial/Print.c
@@ -1,21 +1,30 @@
 // Author : Aaron Naylor
 
-void polyPrint(polyList *poly1)
+//#include <stdlib.h>
+//#include <stdio.h>
+#include "polynomial.h"
+
+/*int main()
 {
-  
-  int power=1; // stores power value
-  int order = polyOrder(poly1); // get highest order that coefficient isn't 1
+  double arr[] = {5, 2, 6, 7};
+  polyList *poly1;
+  poly1 = polyCreate();
+  fillPoly(poly1, 3, arr);
+  polyPrint(poly1);
+}*/
+
+void polyPrint(polyList *poly1)
+{  
+  int order = polyOrder(poly1); // get highest order that coefficient isn't 0
   poly1->current = poly1->head; // set current to first element
-  printf("%lf +", (poly1->current->d.coefficient)); // prints first element
+  printf("%lf", (poly1->current->d.coefficient)); // prints first element
   poly1->current = poly1->current->next; // sets current to next number 
   
-  for(int i=1; i < order; i ++) // for loop from 0 to highest order
-    {
-      
-      printf("%lf x^%d +",(poly1->current->d.coefficient),power); // prints coefficient polynominal starting at least significant
-      power++; // increase power by 1
+  for(int i = 1; i <= order; i ++) // for loop from 0 to highest order
+  {
+      printf(" + %lfx^%d", poly1->current->d.coefficient, i); // prints coefficient polynominal starting at least significant
       poly1->current = poly1->current->next; // sets current to next number 
-    }
-  
-  printf("%lf x^%d\n", (poly1->current->d.coofficient),power); // prints most signicant of polynominal
+  }
+  // Print new line
+  printf("\n\n");
 }
