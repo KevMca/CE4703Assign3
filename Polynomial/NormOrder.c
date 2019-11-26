b14a403599b5126c4521b5852e0edcca5ec6304e
Kevin McAndrew <Kevin@Michaels-MacBook-Pro.local>
2019-11-26 10:40:32 +0000

    tested and completed normalise and order

--- Polynomial/NormOrder.c
+++ Polynomial/NormOrder.c
@@ -1,27 +1,46 @@
-polyList normalise(polyList *poly1){
-  polyList norm; // variable to store value before returned
+// Author: Aaron
+
+//#include <stdio.h>
+//#include <stdlib.h>
+#include "polynomial.h"
+/*
+int main()
+{
+  polyList *polyNew;
+  polyNew = polyCreate();
+  double arr[] = {10, 5, 0, 2, 0};
+  fillPoly(polyNew, 3, arr);
+  //printf("%d\n", polyOrder(polyNew));
+  polyList *polyNorm = polyNormalise(polyNew);
+  printf("%lf\n", polyNorm->current->d.coefficient);
+}*/
+
+polyList *polyNormalise(polyList *poly1){
+  polyList *norm; // variable to store value before returned
   double highCoeff; // stores coefficient of highest order
-  int order; // stores highest order
-  order = order(poly1); // calls order function to find highest order
+  int order = polyOrder(poly1); // calls order function to find highest order
+  poly1->current = poly1->head;
   for(int i =0; i < order; i++) // for loop from 0 to order in increments of 1
     {
-      current = poly1->current->next; // sets current to next value
+      poly1->current = poly1->current->next; // sets current to next value
     }
-
   highCoeff = poly1->current->d.coefficient; // stores coefficient
-  norm =  polyList divide(poly1, highCoeff); // calls dividing function
+  poly1->current = poly1->head;
+  norm = divide(poly1, highCoeff); // calls dividing function
   return norm; // returns coefficient of highest order 
 }
 
-int order(polyList *poly1){
+int polyOrder(polyList *poly1){
   int length; // variable to store highest order in polyList
-  for(int i=0; i =< (poly1->order); i ++) // for loop from 0 to length of polylist 
+  poly1->current = poly1->head;
+  for(int i=0; i <= (poly1->order); i ++) // for loop from 0 to length of polylist 
     {
       if(poly1->current->d.coefficient !=0) // if poly1's coefficient is not equal to 0 
 	{
 	  length = i; // length is highest order that is not equal to 0
 	} 
-      current = poly1->current->next; // sets current to next value
+      poly1->current = poly1->current->next; // sets current to next value
     }
+  poly1->current = poly1->head;
   return length; // returns most significant order
 }
