1a7b14fb4302e3d94429947b328043a53fbf93ee
Kevin McAndrew <Kevin@Michaels-MacBook-Pro.local>
2019-11-28 22:16:07 +0000

    added multiply test

--- Polynomial/polyOpTest.c
+++ Polynomial/polyOpTest.c
@@ -58,9 +58,18 @@ int main(int argc, char **argv)
 		"a coefficient or order wasn't as expected", 
 		"polyAdd()", 
 		testPolyAdd);
-  	//polyAdd()
-  	//polySubtract()
-  	//multiply()
+
+  	// Test polySubtract ////////////////////////////////////
+  	test("all coefficients and order were as expected", 
+		"a coefficient or order wasn't as expected", 
+		"polySubtract()", 
+		testPolySubtract);
+
+  	// Test polyMultiply ////////////////////////////////////
+  	test("all coefficients and order were as expected", 
+		"a coefficient or order wasn't as expected", 
+		"polyMultiply()", 
+		testPolyMultiply);
   	
 	// Test divide //////////////////////////////////////////
   	/*passed = 0;
@@ -127,10 +136,13 @@ result testPolyAdd()
 		if(poly3->current->d.coefficient != arrFill1[i] + arrFill2[i]
 			|| poly3->current->d.order != i)
 		{
+			// Print current iteration, order and coefficient
+			printf("\niter: %d\norder:%d\ncoefficient:%lf", i, 
+				poly3->current->d.order, poly3->current->d.coefficient);
 			// Coefficient not as expected
-			free(poly1);
-			free(poly2);
-			free(poly3);
+			polyDelete(poly1);
+			polyDelete(poly2);
+			polyDelete(poly3);
 			return FAILED;
 		}
 		// Increment current cursors
@@ -140,8 +152,105 @@ result testPolyAdd()
 	}
 
 	// All coefficients were as expected
-	free(poly1);
-	free(poly2);
-	free(poly3);
+	polyDelete(poly1);
+	polyDelete(poly2);
+	polyDelete(poly3);
+	return PASSED;
+}
+
+/////////////////////////////////////////////////////////
+// Test polySubtract
+/////////////////////////////////////////////////////////
+result testPolySubtract()
+{
+	// Create polynomials
+	polyList *poly1 = polyCreate();
+	polyList *poly2 = polyCreate();
+	polyList *poly3 = polyCreate();
+	// Create arr to fill polynomials
+	double arrFill1[] = {0, 1, 2, 3};
+	double arrFill2[] = {3, 2, 1, 0};
+	int order = 3;
+	// Fill arrrays
+	fillPoly(poly1, order, arrFill1);
+	fillPoly(poly2, order, arrFill2);
+
+	// Run polyAdd
+	poly3 = polySubtract(poly1, poly2);
+
+	// Reset current cursor
+	polyToHead(poly1);
+	polyToHead(poly2);
+	polyToHead(poly3);
+	// Check each coefficient and order
+	for(int i = 0; i <=order; i++)
+	{
+		if(poly3->current->d.coefficient != arrFill1[i] - arrFill2[i]
+			|| poly3->current->d.order != i)
+		{
+			// Print current iteration, order and coefficient
+			printf("\niter: %d\norder:%d\ncoefficient:%lf", i, 
+				poly3->current->d.order, poly3->current->d.coefficient);
+			// Coefficient not as expected
+			polyDelete(poly1);
+			polyDelete(poly2);
+			polyDelete(poly3);
+			return FAILED;
+		}
+		// Increment current cursors
+		poly3->current = poly3->current->next;
+		poly2->current = poly2->current->next;
+		poly1->current = poly1->current->next;
+	}
+
+	// All coefficients were as expected
+	polyDelete(poly1);
+	polyDelete(poly2);
+	polyDelete(poly3);
+	return PASSED;
+}
+
+/////////////////////////////////////////////////////////
+// Test polyMultiply
+/////////////////////////////////////////////////////////
+result testPolyMultiply()
+{
+	// Create polynomials
+	polyList *poly1 = polyCreate();
+	polyList *poly2 = polyCreate();
+	// Create arr to fill polynomials
+	double arrFill1[] = {0, 1, 2, 3};
+	int order = 3;
+	// Fill arrrays
+	fillPoly(poly1, order, arrFill1);
+
+	// Run polyAdd
+	poly2 = polyMultiply(poly1, 2);
+
+	// Reset current cursor
+	polyToHead(poly1);
+	polyToHead(poly2);
+	// Check each coefficient and order
+	for(int i = 0; i <=order; i++)
+	{
+		if(poly2->current->d.coefficient != arrFill1[i] * 2
+			|| poly2->current->d.order != i)
+		{
+			// Print current iteration, order and coefficient
+			printf("\niter: %d\norder:%d\ncoefficient:%lf", i, 
+				poly2->current->d.order, poly2->current->d.coefficient);
+			// Coefficient not as expected
+			polyDelete(poly1);
+			polyDelete(poly2);
+			return FAILED;
+		}
+		// Increment current cursors
+		poly2->current = poly2->current->next;
+		poly1->current = poly1->current->next;
+	}
+
+	// All coefficients were as expected
+	polyDelete(poly1);
+	polyDelete(poly2);
 	return PASSED;
 }
